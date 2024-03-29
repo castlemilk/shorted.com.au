27ad07c3180d6fd05a65b8384718d9dd7337d74b
Ben Ebsworth <ben.ebsworth@gmail.com>
2024-03-29 11:39:44 +1100

    using ssr component for data fetch

--- web/src/app/top10.tsx
+++ web/src/app/top10.tsx
@@ -1,42 +1,21 @@
-import { FC, cache } from "react";
+"use client";
+import { PlainMessage } from "@bufbuild/protobuf";
+import { FC } from "react";
+import { GetTopShortsResponse } from "~/gen/shorts/v1alpha1/shorts_pb";
+import { TimeSeriesData } from "~/gen/stocks/v1alpha1/stocks_pb";
 
-import { revalidateTag, unstable_cache } from "next/cache";
-import { getTopShorts } from "~/gen/shorts/v1alpha1/shorts-ShortedStocksService_connectquery";
-
-import { createPromiseClient } from "@connectrpc/connect";
-
-import { createConnectTransport } from "@connectrpc/connect-web";
-
-import { ShortedStocksService } from "~/gen/shorts/v1alpha1/shorts_connect";
-import { useQuery } from "@connectrpc/connect-query";
-
-// const getTopShorts = async (period: string, limit: number) => {
-//   const shortsClient = createPromiseClient(
-//     ShortedStocksService,
-//     createConnectTransport({
-//       baseUrl: "https://shorts-ak2zgjnhlq-km.a.run.app",
-//     }),
-//   );
-//   return shortsClient.getTopShorts({
-//     period: period,
-//     limit: limit,
-//   });
-// };
-
-export const Top10: FC = () => {
-  //   const stocks = await getTopShorts("3m", 10);
-  const { data } = useQuery(getTopShorts);
-
-  return data ? (
-    <div>
-      <h1>Top 10</h1>
-      <ul>
-        {data.timeSeries.map((stock) => (
-          <li key={stock.productCode}>{stock.productCode}</li>
-        ))}
-      </ul>
-    </div>
-  ) : (
-    <div>loading...</div>
-  );
+export const Top10Chart: FC<{ data: PlainMessage<TimeSeriesData>[] }> = ({ data }) => {
+    console.log(data)
+    return data ? (
+        <div>
+            <h1>Top 10</h1>
+            <ul>
+                {data.map((stock: any) => (
+                    <li key={stock.productCode}>{stock.productCode}</li>
+                ))}
+            </ul>
+        </div>
+    ) : (
+        <div>loading...</div>
+    );
 };
