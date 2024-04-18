db834e8b44b2c40aff8af3a297edeb85ca7db989
Ben Ebsworth <ben.ebsworth@gmail.com>
2024-04-18 23:50:40 +1000

    WIP: updating sparklines

--- web/src/app/topShortsView/components/sparkline-v2.tsx
+++ web/src/app/topShortsView/components/sparkline-v2.tsx
@@ -109,7 +109,7 @@ const SparkLine = ({ data }: { data: PlainMessage<TimeSeriesData> }) => {
             width={width}
             height={150}
             data={data}
-            margin={{ top: 10, right: 0, bottom: 0, left: 0 }}
+            margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
           />
         )}
       </ParentSize>
