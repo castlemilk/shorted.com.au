eae32ee64ea92edaf258e1cfbd448fceafa4646d
Ben Ebsworth <ben.ebsworth@gmail.com>
2024-04-21 16:08:36 +1000

    add google analytics

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { getTopShortsData } from "~/app/actions/getTopShorts";
 import { TopShorts } from "./topShortsView/topShorts";
 
 const Page = async () => {
   const data = await getTopShortsData("3m", 10);
   return (
-    <div>
-      <TopShorts initialShortsData={data.timeSeries} />
-    </div>
+    <>
+      <GoogleAnalytics gaId="G-X85RLQ4N2N" />
+      <div>
+        <TopShorts initialShortsData={data.timeSeries} />
+      </div>
+    </>
   );
 };
 
