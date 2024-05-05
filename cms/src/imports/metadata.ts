b16df8699bb865510ced68f5a30b3d4a5c9bee5e
Ben Ebsworth <ben.ebsworth@gmail.com>
2024-05-05 20:13:01 +1000

    update sync utils

--- cms/src/imports/metadata.ts
+++ cms/src/imports/metadata.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
 import csv from "csvtojson";
+import * as fs from "fs";
 import payload from "payload";
 import { Metadata } from "../payload-types";
 import buildConfig from "../payload.config";
@@ -22,52 +23,50 @@ payload.init({
     );
     const fromCSV: Metadata[] = await csv().fromFile(csvPath);
 
-    const creationPromises = fromCSV.map((source: Metadata) => {
-      return (async () => {
-        const data: Omit<Metadata, "id" | "createdAt" | "updatedAt"> = {
-          stock_code: source.stock_code,
-          company_name: source.company_name,
-          market_cap: source.market_cap,
-          industry: source.industry,
-          listing_date: source.listing_date,
-          details: source.details,
-          summary: source.summary,
-          address: source.address,
-          website: source.website,
-          links: source.links,
-          images: source.images,
-          company_logo_link: source.company_logo_link,
-          // other fields you might have
-        };
-        // eslint-disable-next-line no-await-in-loop
-
-        const createPayload = {
-          collection: "metadata",
-          overrideAccess: true,
-          locale: "en",
-          showHiddenFields: false,
-          // If creating verification-enabled auth doc,
-          // you can optionally disable the email that is auto-sent
-          disableVerificationEmail: true,
-          data,
-          filePath: null,
-        };
-        const mediaCreatePayload = {
-          collection: "media",
-          overrideAccess: true,
-          locale: "en",
-          showHiddenFields: false,
-          // If creating verification-enabled auth doc,
-          // you can optionally disable the email that is auto-sent
-          disableVerificationEmail: true,
-          data: { alt: source.company_name },
-          filePath: null,
-        };
-        if (
-          source.company_logo_link &&
-          source.company_logo_link != "Not Found"
-        ) {
-          createPayload.filePath = path.resolve(
+    const creationPromises = fromCSV
+      // .filter((source: Metadata) => source.stock_code === "SDV")
+      .map((source: Metadata) => {
+        return (async () => {
+          const data: Omit<Metadata, "id" | "createdAt" | "updatedAt"> = {
+            stock_code: source.stock_code,
+            company_name: source.company_name,
+            market_cap: source.market_cap,
+            industry: source.industry,
+            listing_date: source.listing_date,
+            details: source.details,
+            summary: source.summary,
+            address: source.address,
+            website: source.website,
+            links: source.links,
+            images: source.images,
+            company_logo_link: source.company_logo_link,
+            // other fields you might have
+          };
+          // eslint-disable-next-line no-await-in-loop
+          const result = await payload.find({
+            collection: "metadata", // required
+            page: 1,
+            limit: 1,
+            where: {
+              stock_code: { equals: source.stock_code }, // pass a `where` query here
+            },
+          });
+          if (result["docs"].length > 0) {
+            console.log(`metadata for ${source.stock_code} already exists`);
+            return;
+          }
+          const createPayload = {
+            collection: "metadata",
+            overrideAccess: true,
+            locale: "en",
+            showHiddenFields: false,
+            // If creating verification-enabled auth doc,
+            // you can optionally disable the email that is auto-sent
+            disableVerificationEmail: true,
+            data,
+            filePath: null,
+          };
+          const imagePath = path.resolve(
             __dirname,
             `../../../analysis/data/images/${
               isNumeric(source.stock_code.at(0))
@@ -75,23 +74,51 @@ payload.init({
                 : source.stock_code.at(0).toUpperCase()
             }/${source.stock_code}/${source.stock_code}.png`
           );
-          mediaCreatePayload.filePath = createPayload.filePath;
-        }
+          if (
+            source.company_logo_link &&
+            source.company_logo_link != "Not Found"
+          ) {
+            if (fs.existsSync(imagePath)) {
+              createPayload.filePath = imagePath;
+            } else {
+              fs.copyFileSync(
+                path.resolve(
+                  __dirname,
+                  `../../../analysis/data/images/asx-header-logo.png`
+                ),
+                imagePath
+              );
+              createPayload.filePath = imagePath;
+            }
+          }
 
-        try {
-          const resp = await payload.create(createPayload);
-          console.log("created: ", resp);
-          return resp;
-        } catch (e) {
-          console.log("Failed to create payload for data:", source);
-          console.error("Failed to create payload for:", source.stock_code, e);
-          // return a special value or throw to signal failure
-          throw e;
-        }
-      })();
+          if (!createPayload.filePath) {
+            fs.copyFileSync(
+              path.resolve(
+                __dirname,
+                `../../../analysis/data/images/asx-header-logo.png`
+              ),
+              imagePath
+            );
+            createPayload.filePath = imagePath;
+          }
 
-      
-    });
+          try {
+            const resp = await payload.create(createPayload);
+            console.log("created: ", resp);
+            return resp;
+          } catch (e) {
+            console.log("Failed to create payload for data:", source);
+            console.error(
+              "Failed to create payload for:",
+              source.stock_code,
+              e
+            );
+            // return a special value or throw to signal failure
+            throw e;
+          }
+        })();
+      });
     Promise.allSettled(creationPromises).then((results) => {
       results.forEach((result, index) => {
         if (result.status === "fulfilled") {
@@ -106,6 +133,5 @@ payload.init({
       });
       process.exit(0);
     });
-
   },
 });
