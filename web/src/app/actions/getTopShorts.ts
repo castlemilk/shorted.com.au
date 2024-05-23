9d334bd699e16cbcdb75a4bad01d3675f205d522
Ben Ebsworth <ben.ebsworth@gmail.com>
2024-05-23 20:20:10 +1000

    fix rendering

--- web/src/app/actions/getTopShorts.ts
+++ web/src/app/actions/getTopShorts.ts
@@ -2,20 +2,23 @@ import { createConnectTransport } from "@connectrpc/connect-web";
 import { createPromiseClient } from "@connectrpc/connect";
 import { toPlainMessage } from "@bufbuild/protobuf";
 import { ShortedStocksService } from "~/gen/shorts/v1alpha1/shorts_connect";
+import { cache } from "react";
 
-export const getTopShortsData = async (period: string, limit: number, offset: number) => {
-  const transport = createConnectTransport({
-    // All transports accept a custom fetch implementation.
-    fetch,
-    // With Svelte's custom fetch function, we could alternatively
-    // use a relative base URL here.
-    baseUrl:
-      process.env.NEXT_PUBLIC_SHORTS_SERVICE_ENDPOINT ??
-      "http://localhost:8080",
-  });
-  const client = createPromiseClient(ShortedStocksService, transport);
+export const getTopShortsData = cache(
+  async (period: string, limit: number, offset: number) => {
+    const transport = createConnectTransport({
+      // All transports accept a custom fetch implementation.
+      fetch,
+      // With Svelte's custom fetch function, we could alternatively
+      // use a relative base URL here.
+      baseUrl:
+        process.env.NEXT_PUBLIC_SHORTS_SERVICE_ENDPOINT ??
+        "http://localhost:8080",
+    });
+    const client = createPromiseClient(ShortedStocksService, transport);
 
-  const response = await client.getTopShorts({ period, limit, offset });
+    const response = await client.getTopShorts({ period, limit, offset });
 
-  return toPlainMessage(response);
-};
+    return toPlainMessage(response);
+  },
+);
