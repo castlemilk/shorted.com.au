4426d4b31ac4efe7308385ebe205541dd843875e
Ben Ebsworth <ben.ebsworth@gmail.com>
2024-05-23 20:20:10 +1000

    fix rendering

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -6,6 +6,7 @@ import { getIndustryTreeMap } from "./actions/getIndustryTreeMap";
 import { IndustryTreeMapView } from "./treemap/treeMap";
 import { ViewMode } from "~/gen/shorts/v1alpha1/shorts_pb";
 
+export const revalidate = 60 // revalidate the data at most every hour
 const Page = async () => {
   const data = await getTopShortsData("3m", 10, 0);
   const treeMapData = await getIndustryTreeMap("3m", 10, ViewMode.CURRENT_CHANGE)
