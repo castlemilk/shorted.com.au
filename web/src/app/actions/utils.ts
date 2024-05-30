03dde4cb85f4678943d292294e595837bf764c2d
Ben Ebsworth <ben.ebsworth@gmail.com>
2024-05-30 21:18:08 +1000

    fix environment variable

--- web/src/app/actions/utils.ts
+++ web/src/app/actions/utils.ts
@@ -8,7 +8,7 @@ async function getServerAccessToken() {
         projectId: process.env.GOOGLE_PROJECT_ID,
         credentials: {
             client_email: process.env.GOOGLE_CLIENT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+            private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/gm, '\n'),
         },
     });
   
