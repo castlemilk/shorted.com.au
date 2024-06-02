4a0edf8e19482ae24521aff067739ed889c151b6
Ben Ebsworth <ben.ebsworth@gmail.com>
2024-06-02 20:46:39 +1000

    fix auth for other API's

--- web/src/app/actions/getStock.ts
+++ web/src/app/actions/getStock.ts
@@ -4,13 +4,22 @@ import { type PlainMessage, toPlainMessage } from "@bufbuild/protobuf";
 import { ShortedStocksService } from "~/gen/shorts/v1alpha1/shorts_connect";
 import { type Stock } from "~/gen/stocks/v1alpha1/stocks_pb";
 import { cache } from "react";
+import { getAuthorizationHeader } from "./utils";
 
 export const getStock = cache(async (
   productCode: string,
+  token?: string,
 ): Promise<PlainMessage<Stock>> => {
+  const authHeader = await getAuthorizationHeader(new Headers(), token);
   const transport = createConnectTransport({
     // All transports accept a custom fetch implementation.
-    fetch,
+    fetch: (input, init: RequestInit | undefined) => {
+      if (init?.headers) {
+        const headers = init.headers as Headers;
+        headers.set('Authorization', authHeader.get('Authorization') ?? '');
+      }
+      return fetch(input, init);
+    },
     // With Svelte's custom fetch function, we could alternatively\
     // use a relative base URL here.
     baseUrl:
