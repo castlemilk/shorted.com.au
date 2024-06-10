faf753359ff536d7ac56f4b4a5a42d71298626a9
Ben Ebsworth <ben.ebsworth@gmail.com>
2024-06-10 20:11:38 +1000

    move fetches to components

--- web/src/@/components/ui/chart.tsx
+++ web/src/@/components/ui/chart.tsx
@@ -1,79 +1,88 @@
 "use client";
 import ParentSize from "@visx/responsive/lib/components/ParentSize";
 import BrushChart, { type HandleBrushClearAndReset } from "./brushChart";
-import { type TimeSeriesData } from "~/gen/stocks/v1alpha1/stocks_pb";
-import { type PlainMessage } from "@bufbuild/protobuf";
-import { Suspense, useEffect, useRef, useState } from "react";
+import { Suspense, useRef, useState } from "react";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-import { getStockData } from "~/app/actions/getStockData";
 import { Button } from "./button";
 import { Skeleton } from "./skeleton";
+import { useStockData } from "./../../hooks/use-stock-data"; // Custom hook
 
 const INITIAL_PERIOD = "6m";
 export type ChartProps = {
   stockCode: string;
-  initialData: PlainMessage<TimeSeriesData>;
 };
-const Chart = ({ stockCode, initialData }: ChartProps) => {
+
+const Chart = ({ stockCode }: ChartProps) => {
   const [period, setPeriod] = useState<string>(INITIAL_PERIOD);
   const chartRef = useRef<HandleBrushClearAndReset>(null);
-  const [data, setData] = useState<PlainMessage<TimeSeriesData>>(initialData);
-  useEffect(() => {
-    getStockData(stockCode, period)
-      .then((data) => {
-        setData(data);
-      })
-      .catch(console.error);
-  }, [period]);
+  const { data, loading, error } = useStockData(stockCode, period);
+
   const handleClearClick = () => {
     chartRef?.current?.clear();
   };
+
   const handleResetClick = () => {
     chartRef?.current?.reset();
   };
+
   return (
-    <div className="grid">
-      <div className="flex flex-row-reverse">
-        <div className="flex">
-          <Button className="mr-1" size="sm" onClick={handleClearClick}>
-            Clear
-          </Button>
-          <Button size="sm" onClick={handleResetClick}>
-            Reset
-          </Button>
+    <Suspense fallback={<ChartLoadingPlaceholder />}>
+      <div className="grid">
+        <div className="flex flex-row-reverse">
+          <div className="flex">
+            <Button className="mr-1" size="sm" onClick={handleClearClick}>
+              Clear
+            </Button>
+            <Button size="sm" onClick={handleResetClick}>
+              Reset
+            </Button>
+          </div>
+          <div>
+            <ToggleGroup
+              type="single"
+              onValueChange={(v: string) => setPeriod(v)}
+            >
+              <ToggleGroupItem value="1m">1M</ToggleGroupItem>
+              <ToggleGroupItem value="3m">3M</ToggleGroupItem>
+              <ToggleGroupItem value="6m">6M</ToggleGroupItem>
+              <ToggleGroupItem value="1y">1Y</ToggleGroupItem>
+              <ToggleGroupItem value="2y">2Y</ToggleGroupItem>
+              <ToggleGroupItem value="max">max</ToggleGroupItem>
+            </ToggleGroup>
+          </div>
         </div>
-        <div>
-          <ToggleGroup
-            type="single"
-            onValueChange={(v: string) => setPeriod(v)}
-          >
-            <ToggleGroupItem value="1m">1M</ToggleGroupItem>
-            <ToggleGroupItem value="3m">3M</ToggleGroupItem>
-            <ToggleGroupItem value="6m">6M</ToggleGroupItem>
-            <ToggleGroupItem value="1y">1Y</ToggleGroupItem>
-            <ToggleGroupItem value="2y">2Y</ToggleGroupItem>
-            <ToggleGroupItem value="max">max</ToggleGroupItem>
-          </ToggleGroup>
-        </div>
+
+        {(loading || data == null)  ? (
+          <ChartLoadingPlaceholder />
+        ) : error ? (
+          <div>Error loading data</div>
+        ) : (
+          <ParentSize className="min-w-0">
+            {({ width }) => (
+              <BrushChart ref={chartRef} data={data} width={width} height={400} />
+            )}
+          </ParentSize>
+        )}
       </div>
-      <Suspense fallback={<ChartLoadingPlaceholder />}>
-        <ParentSize className="min-w-0">
-          {({ width }) => (
-            <BrushChart ref={chartRef} data={data} width={width} height={400} />
-          )}
-        </ParentSize>
-      </Suspense>
-    </div>
+    </Suspense>
   );
 };
 
 const ChartLoadingPlaceholder = () => (
-  <div className="flex flex-col space-y-3">
-    <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-    <div className="space-y-2">
-      <Skeleton className="h-4 w-[250px]" />
-      <Skeleton className="h-4 w-[200px]" />
+  <div className="grid">
+    <div className="flex flex-row-reverse">
+      <div className="flex">
+        <Skeleton className="h-[40px] w-[60px] ml-2" />
+        <Skeleton className="h-[40px] w-[60px] ml-2" />
+      </div>
+      <div>
+        <Skeleton className="h-[40px] w-[300px]" />
+      </div>
+    </div>
+    <div>
+      <Skeleton className="h-[400px] w-full mt-2" />
     </div>
   </div>
 );
+
 export default Chart;
---
+++ web/src/@/components/ui/companyInfo.tsx
@@ -0,0 +1,74 @@
+import { getStockDetails } from "~/app/actions/getStockDetails";
+import { Card, CardContent, CardHeader, CardTitle } from "./card";
+import { PanelTopIcon } from "lucide-react";
+import Link from "next/link";
+import { Separator } from "./separator";
+import { Suspense } from "react";
+import { Skeleton } from "./skeleton";
+
+const placeHolder = (
+  <Card className="sm:col-span-4">
+    <CardHeader className="pb-3">
+      <CardTitle className="flex">About</CardTitle>
+      <Separator />
+    
+    <CardContent className="p-0">
+      <div className="flex content-center justify-between">
+        <div className="flex content-center">
+          <div className="flex self-center p-2">
+            <PanelTopIcon size={10} />
+          </div>
+          <p className="uppercase font-semibold content-center text-xs">
+            website
+          </p>
+        </div>
+        <span className="flex items-end content-center p-2 text-xs">
+          <Skeleton className="w-[200px] h-[16px]" />
+        </span>
+      </div>
+    </CardContent>
+    <Separator />
+    </CardHeader>
+  </Card>
+);
+
+const companyInfo = async ({ stockCode }: { stockCode: string }) => {
+  const stockDetails = await getStockDetails(stockCode);
+  return (
+    <Suspense fallback={placeHolder}>
+      <Card className="sm:col-span-4">
+        <CardHeader className="pb-3">
+          <CardTitle className="flex">About</CardTitle>
+        
+        <Separator />
+        {stockDetails?.website && (
+          <>
+            <CardContent className="p-0">
+              <div className="flex content-center justify-between">
+                <div className="flex content-center">
+                  <div className="flex self-center p-2">
+                    <PanelTopIcon size={10} />
+                  </div>
+                  <p className="uppercase font-semibold content-center text-xs">
+                    website
+                  </p>
+                </div>
+                <span className="flex items-end content-center p-2 text-xs">
+                  <Link href={stockDetails?.website}>
+                    <p className="text-blue-600">
+                      {stockDetails?.website.split(/www.|\:\/\//).at(-1)}
+                    </p>
+                  </Link>
+                </span>
+              </div>
+            </CardContent>
+            <Separator />
+          </>
+        )}
+        </CardHeader>
+      </Card>
+    </Suspense>
+  );
+};
+
+export default companyInfo;
