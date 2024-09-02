b0cf9b46bdbf878963238d836c6037f3e13602c1
Ben Ebsworth <ben.ebsworth@gmail.com>
2024-09-02 23:39:27 +1000

    initial scaffolding for blog

--- web/src/@/components/ui/site-header.tsx
+++ web/src/@/components/ui/site-header.tsx
@@ -6,7 +6,7 @@ const SiteHeader: FC = () => {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="pr-5 pl-5 flex w-full h-14 items-center">
-        <MainNav items={[{ title: "about", href: "/about" }]} />
+        <MainNav items={[{ title: "about", href: "/about" }, { title: "blog", href: "/blog" }]} />
         <div className="flex flex-1 items-center space-x-2 justify-end">
           <nav className="flex items-center">
             <ModeToggle />
