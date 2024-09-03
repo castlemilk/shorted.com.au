eb339b32d7698ee49a8aa2304868e5c6538213ad
Ben Ebsworth <ben.ebsworth@gmail.com>
2024-09-03 23:37:51 +1000

    fix rendering

--- web/src/@/components/ui/site-header.tsx
+++ web/src/@/components/ui/site-header.tsx
@@ -1,12 +1,15 @@
 import { type FC } from "react";
 import { MainNav } from "./main-nav";
 import { ModeToggle } from "./mode-toggle";
-
+const items = [
+  { title: "about", href: "/about" },
+  { title: "blog", href: "/blog" },
+];
 const SiteHeader: FC = () => {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="pr-5 pl-5 flex w-full h-14 items-center">
-        <MainNav items={[{ title: "about", href: "/about" }, { title: "blog", href: "/blog" }]} />
+        <MainNav />
         <div className="flex flex-1 items-center space-x-2 justify-end">
           <nav className="flex items-center">
             <ModeToggle />
