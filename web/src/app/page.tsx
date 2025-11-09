d36eb5a20386b91f64694ae94bb7bb234cc0fb17
Ben Ebsworth <ben.ebsworth@gmail.com>
2025-11-09 21:19:20 +1030

    update tests

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,23 +1,15 @@
+"use client";
+
 import React from "react";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import { getTopShortsData } from "~/app/actions/getTopShorts";
 import { TopShorts } from "./topShortsView/topShorts";
-import { getIndustryTreeMap } from "./actions/getIndustryTreeMap";
 import { IndustryTreeMapView } from "./treemap/treeMap";
 import { ViewMode } from "~/gen/shorts/v1alpha1/shorts_pb";
-import { auth } from "~/server/auth";
 import { LoginPromptBanner } from "~/@/components/ui/login-prompt-banner";
+import { useSession } from "next-auth/react";
 
-export const revalidate = 60; // revalidate the data at most every minute
-
-const Page = async () => {
-  const session = await auth();
-  const data = await getTopShortsData("3m", 10, 0);
-  const treeMapData = await getIndustryTreeMap(
-    "3m",
-    10,
-    ViewMode.CURRENT_CHANGE,
-  );
+const Page = () => {
+  const { data: session } = useSession();
 
   return (
     <>
@@ -28,11 +20,10 @@ const Page = async () => {
       {/* Main dashboard view - accessible to all users */}
       <div className="flex flex-col lg:flex-row">
         <div className="lg:w-2/5">
-          <TopShorts initialShortsData={data.timeSeries} initialPeriod="3m" />
+          <TopShorts initialPeriod="3m" />
         </div>
         <div className="lg:w-3/5">
           <IndustryTreeMapView
-            initialTreeMapData={treeMapData}
             initialPeriod="3m"
             initialViewMode={ViewMode.CURRENT_CHANGE}
           />
