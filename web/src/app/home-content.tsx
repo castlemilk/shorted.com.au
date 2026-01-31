b9052519154646874ee11d7fe5a503bf0362ee85
Ben Ebsworth <ben.ebsworth@gmail.com>
2026-01-31 23:38:39 +1100

    fix: restore industry treemap to homepage
    The treemap was removed during SSR debugging. This restores the
    side-by-side layout with TopShorts (2/5) and IndustryTreeMapView (3/5).
    Updated tests to match new HomeContent structure.

--- web/src/app/home-content.tsx
+++ web/src/app/home-content.tsx
@@ -2,6 +2,7 @@
 
 import dynamic from "next/dynamic";
 import { Skeleton } from "~/@/components/ui/skeleton";
+import { ViewMode } from "~/gen/shorts/v1alpha1/shorts_pb";
 
 const TopShorts = dynamic(
   () => import("./topShortsView/topShorts").then((mod) => mod.TopShorts),
@@ -15,10 +16,32 @@ const TopShorts = dynamic(
   }
 );
 
+const IndustryTreeMapView = dynamic(
+  () => import("./treemap/treeMap").then((mod) => mod.IndustryTreeMapView),
+  {
+    loading: () => (
+      <div className="p-4">
+        <Skeleton className="h-[700px] w-full rounded-xl" />
+      </div>
+    ),
+    ssr: false,
+  }
+);
+
 export function HomeContent() {
   return (
     <div className="container mx-auto px-4 py-4">
-      <TopShorts initialPeriod="3m" />
+      <div className="flex flex-col lg:flex-row">
+        <div className="lg:w-2/5">
+          <TopShorts initialPeriod="3m" />
+        </div>
+        <div className="lg:w-3/5">
+          <IndustryTreeMapView
+            initialPeriod="3m"
+            initialViewMode={ViewMode.CURRENT_CHANGE}
+          />
+        </div>
+      </div>
     </div>
   );
 }
