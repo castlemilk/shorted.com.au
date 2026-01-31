fdd8e11944d2c303f34cfe803033923aa4037746
Ben Ebsworth <ben.ebsworth@gmail.com>
2026-01-31 16:38:04 +1100

    debug: remove optional components from HomeContent

--- web/src/app/home-content.tsx
+++ web/src/app/home-content.tsx
@@ -1,23 +1,13 @@
 "use client";
 
 import React from "react";
-import { GoogleAnalytics } from "@next/third-parties/google";
 import { TopShorts } from "./topShortsView/topShorts";
 import { IndustryTreeMapView } from "./treemap/treeMap";
 import { ViewMode } from "~/gen/shorts/v1alpha1/shorts_pb";
-import { LoginPromptBanner } from "~/@/components/ui/login-prompt-banner";
-import { useSession } from "next-auth/react";
 
 export function HomeContent() {
-  const { data: session } = useSession();
-
   return (
     <div className="container mx-auto px-4 py-4">
-      <GoogleAnalytics gaId="G-X85RLQ4N2N" />
-
-      {/* Login prompt banner for non-authenticated users */}
-      {!session && <LoginPromptBanner />}
-
       {/* Main dashboard view */}
       <div className="grid grid-cols-1 lg:grid-cols-[2fr_3fr] gap-8">
         <div className="min-w-0">
